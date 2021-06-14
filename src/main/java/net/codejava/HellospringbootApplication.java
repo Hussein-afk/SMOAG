ee05936d531c56067c9c9d39aa4242b4e30c5eba
Hussein Bassal <habassal99@gmail.com>
2021-06-14 09:48:17 +0300

    make it better

---
+++ src/main/java/net/codejava/HellospringbootApplication.java
@@ -0,0 +1,23 @@
+package net.codejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class HellospringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HellospringbootApplication.class, args);
+    }
+
+}
+/*
+@RestController
+class HelloWorld{
+    @GetMapping("/")
+    String hello(){
+        return "Hello World";
+    }
+}*/
